a98f4072a7601c34a08d56cd919addc740d2b292
Salim Al Sazu <accsalimalsazu@gmail.com>
2023-07-23 01:05:10 +0600

    Explore the World Section Done

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "./hero/HeroSection";
+import ExploreTheWorld from "./exploreworld/ExploreTheWorld";
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection />
+      <ExploreTheWorld />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/home/hero/HeroSection.jsx
@@ -0,0 +1,23 @@
+import heroImg from "../../../assets/image/home/Frame.png";
+import "./heroSection.css";
+
+const HeroSection = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex items-center mx-[135px]">
+        <div className="w-auto h-[175px]">
+          <h1 className="hero-p text-[70px]">
+            Amazing{" "}
+            <span className="adventure text-[70px] mr-2">adventure</span> <br />
+            waiting for you!
+          </h1>
+        </div>
+        <div>
+          <img src={heroImg} alt="" className="w-[430px] h-[430px] ml-[8px]" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
