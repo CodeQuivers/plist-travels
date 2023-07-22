4f3cb76f47cb2b7238c2503cacf94a8b428c5229
MashodRana <mashod0rana@gmail.com>
2023-07-22 18:35:41 +0600

    design for tour serach component

---
+++ src/pages/tours/Tours.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SearchTour from '../../components/tours/SearchTour'
+import './Tours.css'
+
+const Tours = () => {
+  return (
+    <div className="
+    xl:container
+    mx-auto
+    ">
+        <SearchTour/>
+    </div>
+  )
+}
+
+export default Tours
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 // import App from "../App";
 import Main from "../layout/Main";
+import Tours from "../pages/tours/Tours";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    children:[
+      {
+        path:'tours',
+        element:<Tours/>
+      }
+    ]
   },
 ]);
 
