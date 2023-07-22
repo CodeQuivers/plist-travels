2391cf3a11b40d6ab2476039769f6c7aabc4d4a3
Salim Al Sazu <accsalimalsazu@gmail.com>
2023-07-22 11:48:33 +0600

    router provider is changed from main to app

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/router.jsx";
 import React from "react";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
