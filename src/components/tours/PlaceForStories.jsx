e16e18f77c9e9b04e8cd815bba6d2e0687b6b173
MashodRana <mashod0rana@gmail.com>
2023-07-23 02:10:08 +0600

    stories started

---
+++ src/components/tours/PlaceForStories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlaceForStories = () => {
+  return (
+    <div className="xl:container mx-auto px-12 mt-5 sm:mt-8 lg:mt-[72px]">
+        
+    </div>
+  )
+}
+
+export default PlaceForStories;
