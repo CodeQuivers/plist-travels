d4d7664d2c79c936c8faf8c6bbae798b227ad5ad
shourovr82 <shafinur512@gmail.com>
2023-07-23 08:59:17 +0600

    [ALMOST] hotel page almost completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import router from "./routes/router";
 
 const App = () => {
   return (
-    <div>
+    <div >
       <RouterProvider router={router} />
     </div>
   );
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-// import App from "../App";
 import Main from "../layout/Main";
 import Flight from "../pages/flight/Flight";
 
