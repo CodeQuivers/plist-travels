1316e1f659e938c1f81c72d4d3359ca3627148f6
Salim Al Sazu <accsalimalsazu@gmail.com>
2023-07-23 17:13:35 +0600

    exlpore nigeria section done

--- src/components/home/explorenigeria/exploreNigeria.jsx
+++ src/components/home/explorenigeria/exploreNigeria.jsx
@@ -27,9 +27,9 @@ const ExploreNigeria = () => {
       </div>
 
       {/* Card Section */}
-      <div className="h-[381px] w-[265px] flex items-center gap-[36px] ">
+      <div className="h-[381px]  flex items-center gap-[33px] ">
         {/* card 1 */}
-        <div className="flex flex-col rounded-lg">
+        <div className="flex flex-col  rounded-lg">
           <div className="relative">
             <img src={imgae1} alt="" className="rounded-t-lg" />
             <button className="absolute top-[21px] right-[21px]">
@@ -59,7 +59,7 @@ const ExploreNigeria = () => {
           </div>
         </div>
         {/* card 2 */}
-        <div className="flex flex-col rounded-lg">
+        <div className="flex flex-col  rounded-lg">
           <div className="relative">
             <img src={imgae2} alt="" className="rounded-t-lg" />
             <button className="absolute top-[21px] right-[21px]">
@@ -89,7 +89,7 @@ const ExploreNigeria = () => {
           </div>
         </div>
         {/* card 3 */}
-        <div className="flex flex-col rounded-lg">
+        <div className="flex flex-col  rounded-lg">
           <div className="relative">
             <img src={imgae3} alt="" className="rounded-t-lg" />
             <button className="absolute top-[21px] right-[21px]">
@@ -119,25 +119,25 @@ const ExploreNigeria = () => {
           </div>
         </div>
         {/* card 4 */}
-        <div className="flex flex-col rounded-lg">
+        <div className="flex flex-col border border-blue-500 rounded-lg">
           <div className="relative">
             <img src={imgae4} alt="" className="rounded-t-lg" />
             <button className="absolute top-[21px] right-[21px]">
               <img src={heart} alt="" className="bg-white rounded-lg p-[6px]" />
             </button>
           </div>
-          <div className="explore-nigeria-card flex flex-col items-start h-[141px] w-[267px]">
+          <div className="explore-nigeria-card1 flex flex-col items-start h-[141px] w-[267px]">
             <div className="flex items-start ">
               <div>
-                <h3 className="explore-nigeria-name">The grand resort</h3>
-                <p className="explore-nigera-location">Karineside</p>
+                <h3 className="explore-nigeria-name1">The grand resort</h3>
+                <p className="explore-nigera-location1">Karineside</p>
               </div>
               <div>
-                <p className="ml-[40px] explore-nigeria-price ">$548</p>
+                <p className="ml-[40px] explore-nigeria-price1 ">$548</p>
               </div>
             </div>
             <hr className="text-white border-[1px] w-full" />
-            <div className="flex items-start explore-nigeria-date">
+            <div className="flex items-start explore-nigeria-date1">
               <div>
                 <span>Tue, Jul 20</span> - <span>Fri, Jul 23</span>
               </div>
