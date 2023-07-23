a0b81aae2f8fcaa7bfd76fa9b629c7a24236a099
Md Rafiqul Islame <rafiqulislam11002@gmail.comm>
2023-07-23 23:42:01 +0600

    transfer page component added and left faq section

---
+++ src/components/transfer/TransferSearchForm/TransferSearchForm.jsx
@@ -0,0 +1,113 @@
+import DatePicker from "react-datepicker";
+import { useState } from "react";
+import DropDown from "../TransferDropDown/TransferDropDown";
+
+const TransferSearchForm = () => {
+    const [startDate, setStartDate] = useState(new Date());
+    const [pickUpTime, setPickUpTime] = useState(new Date());
+    console.log('pickUpTime', pickUpTime);
+    const [returnTime, setReturnTime] = useState(new Date());
+    console.log('returnTime', returnTime);
+    
+    return (
+        <div className="h-[484px] mt-16 pl-[42px] rounded-lg border-2 border-indigo-800 pt-[90px]">
+                <div className="inline-flex items-start gap-7">
+                    <div className="flex items-center gap-2">
+                        <input type="radio" name="" id="" />
+                        <p>One way</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <input type="radio" name="" id="" />
+                        <p>Round-Trip</p>
+                    </div>
+                </div>
+
+                {/*  */}
+                <div className="mt-5 flex gap-2">
+                    <div className="w-[265px] pt-[2px] pr-[26px]">
+                        <p>Destination</p>
+                        <div className="relative">
+                            <DropDown />
+                        </div>
+                    </div>
+                    <div className="w-[265px] pt-[2px] pr-[26px]">
+                        <p>Pick Up</p>
+                        <div className="relative">
+                            <DropDown />
+                        </div>
+                    </div>
+                    <div className="w-[265px] pt-[2px] pr-[26px]">
+                        <p>Airport</p>
+                        <div className="relative">
+                            <DropDown />
+                        </div>
+                    </div>
+                    <div className="w-[265px] pt-[2px] pr-[26px]">
+                        <p>Drop Off</p>
+                        <div className="relative">
+                            <DropDown />
+                        </div>
+                    </div>
+                </div>
+
+                {/*  */}
+                <div>
+                    <div className="mt-5 flex gap-2">
+                        <div className="w-[265px] pt-[2px] pr-7">
+                            <p>Accomodation</p>
+                            <DropDown />
+                        </div>
+                        <div className="w-[265px] pt-[2px] pr-7">
+                            <p>Pick Up Date</p>
+                            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                        </div>
+                        <div className="w-[265px] pt-[2px] pr-7">
+                            <p>Pick up Time</p>
+                            <DatePicker
+                                selected={pickUpTime}
+                                onChange={(date) => setPickUpTime(date)}
+                                showTimeSelect
+                                showTimeSelectOnly
+                                timeIntervals={15}
+                                timeCaption="Time"
+                                dateFormat="h:mm aa"
+                            />
+                        </div>
+                        <div className="w-[265px] pt-[2px] pr-7">
+                            <p>Return Date</p>
+                            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="mt-5 flex gap-2">
+                        <div className="w-[265px] pt-[2px] pr-7">
+                            <p>Return Time</p>
+                            <DatePicker
+                                selected={returnTime}
+                                onChange={(date) => setReturnTime(date)}
+                                showTimeSelect
+                                showTimeSelectOnly
+                                timeIntervals={15}
+                                timeCaption="Time"
+                                dateFormat="h:mm aa"
+                            />
+                        </div>
+                        <div className="w-[265px] pt-[2px] pr-7">
+                            <p>Passengers</p>
+                            <DropDown />
+                        </div>
+                        <div className="flex items-end">
+                            <input
+                                className="w-[250px] h-[47px] old-logo-color text-white rounded-lg"
+                                type="submit"
+                                value="Search Now"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+    );
+};
+
+export default TransferSearchForm;
--- src/pages/transfer/Transfer.jsx
+++ src/pages/transfer/Transfer.jsx
@@ -1,182 +1,37 @@
-import flightFrom from "../../assets/image/flight/icons/FlightFrom.png";
-// import depature from "../../assets/image/flight/icons/depature.png";
-import flightTo from "../../assets/image/flight/icons/flightTo.png";
-// import returnCalender from "../../assets/image/flight/icons/return-calender.png";
-import DropDown from "../../components/transfer/TransferDropDown";
 import Divider from "../../components/tours/Divider";
-// import Card1 from "../../components/flight/Card1";
-import globalWorld from "../../assets/image/flight/icons/globalWorld.svg";
-// import suport from "../../assets/image/flight/icons/Suport.svg";
-// import vast from "../../assets/image/flight/icons/Vast.svg";
-// import partner from "../../assets/image/flight/icons/Partner.svg";
-import DatePicker from "react-datepicker";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import PickDriveFromNazarian from "../../components/transfer/PickDriveFromNazarian/PickDriveFromNazarian";
+import PopularCars from "../../components/transfer/PopularCars/PopularCars";
+import TransferAccordianLists from "../../components/transfer/TransferAccordian/TransferAccordianLists";
+import TransferBanner from "../../components/transfer/TransferBanner/TransferBanner";
+import TransferPagination from "../../components/transfer/TransferPagination/TransferPagination";
+import TransferSearchForm from "../../components/transfer/TransferSearchForm/TransferSearchForm";
 
 const Transfer = () => {
-    const [startDate, setStartDate] = useState(new Date());
-    const [pickUpTime, setPickUpTime] = useState(new Date());
-    console.log('pickUpTime', pickUpTime);
-    const [returnTime, setReturnTime] = useState(new Date());
-    console.log('returnTime', returnTime);
+    
 
     return (
-        <div>
-      <div className="h-[484px] mt-16 pl-[42px] rounded-lg border-2 border-indigo-800 pt-[90px]">
-        <div className="inline-flex items-start gap-7">
-          <div className="flex items-center gap-2">
-            <input type="radio" name="" id="" />
-            <p>One way</p>
-          </div>
-          <div className="flex items-center gap-2">
-            <input type="radio" name="" id="" />
-            <p>Round-Trip</p>
-          </div>
+        <div className="w-auto lg:w-[1170px] mx-auto">
+            <div>
+                <TransferSearchForm/>
+            </div>
+            <Divider />
+            {/* Pick Your Drive From Nazarian   */}
+            <div>
+                <PickDriveFromNazarian/>
+            </div>
+            <div>
+                <TransferBanner/>
+            </div>
+            <div>
+                <PopularCars/>
+            </div>
+            <div>
+                <TransferPagination/>
+            </div>
+            <div>
+                <TransferAccordianLists/>
+            </div>
         </div>
-
-        {/*  */}
-        <div className="mt-5 flex gap-2">
-          <div className="w-[265px] pt-[2px] pr-[26px]">
-            <p>Destination</p>
-            <div className="relative">
-            <DropDown />
-            </div>
-          </div>
-          <div className="w-[265px] pt-[2px] pr-[26px]">
-            <p>Pick Up</p>
-            <div className="relative">
-            <DropDown />            
-            </div>
-          </div>
-          <div className="w-[265px] pt-[2px] pr-[26px]">
-            <p>Airport</p>
-            <div className="relative">
-            <DropDown />
-            </div>
-          </div>
-          <div className="w-[265px] pt-[2px] pr-[26px]">
-            <p>Drop Off</p>
-            <div className="relative">
-            <DropDown />
-            </div>
-          </div>
-        </div>
-
-        {/*  */}
-        <div>
-          <div className="mt-5 flex gap-2">
-            <div className="w-[265px] pt-[2px] pr-7">
-              <p>Accomodation</p>
-              <DropDown />
-            </div>
-            <div className="w-[265px] pt-[2px] pr-7">
-              <p>Pick Up Date</p>
-              <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-            </div>
-            <div className="w-[265px] pt-[2px] pr-7">
-              <p>Pick up Time</p>
-              <DatePicker
-      selected={pickUpTime}
-      onChange={(date) => setPickUpTime(date)}
-      showTimeSelect
-      showTimeSelectOnly
-      timeIntervals={15}
-      timeCaption="Time"
-      dateFormat="h:mm aa"
-    />
-
-            </div>
-            <div className="w-[265px] pt-[2px] pr-7">
-              <p>Return Date</p>
-              <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="mt-5 flex gap-2">
-            <div className="w-[265px] pt-[2px] pr-7">
-              <p>Return Time</p>
-              <DatePicker
-      selected={returnTime}
-      onChange={(date) => setReturnTime(date)}
-      showTimeSelect
-      showTimeSelectOnly
-      timeIntervals={15}
-      timeCaption="Time"
-      dateFormat="h:mm aa"
-    />
-            </div>
-            <div className="w-[265px] pt-[2px] pr-7">
-              <p>Passengers</p>
-              <DropDown />
-            </div>
-            <div className="flex items-end">
-              <input
-                className="w-[250px] h-[47px] old-logo-color text-white rounded-lg"
-                type="submit"
-                value="Search Now"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      <Divider />
-      {/* card */}
-      <div className="flex justify-center gap-6">
-        <div className="flex-col flex rounded-2xl items-center w-[274px] old-logo-color p-6 text-white">
-          <img
-            className="pb-[15px] w-[69px]"
-            src={globalWorld}
-            alt="globalWorld"
-          />
-          <h3 className="pb-[10px]">Travel with Ease</h3>
-          <p className="pb-5">
-            Excepteur sint occae cupidatat non proident, sunt in culpa qui
-            officia.
-          </p>
-          <Link>Explore</Link>
-        </div>
-        <div className="flex-col flex rounded-2xl items-center w-[274px] old-logo-color p-6 text-white">
-          <img
-            className="pb-[15px] w-[69px]"
-            src={globalWorld}
-            alt="globalWorld"
-          />
-          <h3 className="pb-[10px]">Travel with Ease</h3>
-          <p className="pb-5">
-            Excepteur sint occae cupidatat non proident, sunt in culpa qui
-            officia.
-          </p>
-          <Link>Explore</Link>
-        </div>
-        <div className="flex-col flex rounded-2xl items-center w-[274px] old-logo-color p-6 text-white">
-          <img
-            className="pb-[15px] w-[69px]"
-            src={globalWorld}
-            alt="globalWorld"
-          />
-          <h3 className="pb-[10px]">Travel with Ease</h3>
-          <p className="pb-5">
-            Excepteur sint occae cupidatat non proident, sunt in culpa qui
-            officia.
-          </p>
-          <Link>Explore</Link>
-        </div>
-        <div className="flex-col flex rounded-2xl items-center w-[274px] old-logo-color p-6 text-white">
-          <img
-            className="pb-[15px] w-[69px]"
-            src={globalWorld}
-            alt="globalWorld"
-          />
-          <h3 className="pb-[10px]">Travel with Ease</h3>
-          <p className="pb-5">
-            Excepteur sint occae cupidatat non proident, sunt in culpa qui
-            officia.
-          </p>
-          <Link>Explore</Link>
-        </div>
-      </div>
-    </div>
     );
 };
 
