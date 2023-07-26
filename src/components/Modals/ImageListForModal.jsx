7e9052e7da8d1abe026e99198a15b61295695607
MashodRana <mashod0rana@gmail.com>
2023-07-26 23:49:03 +0600

    [done] modal done for the image gallery

---
+++ src/components/Modals/ImageListForModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageListForModal = ({ imgId, setHeroImgId, item }) => {
+  return (
+    <div onClick={() => setHeroImgId(imgId)} className="w-[18%] gard-border-olc hover:cursor-pointer">
+    <img className="w-[194px] h-[166px]" src={item} alt="room details photo" />
+  </div>
+  )
+}
+
+export default ImageListForModal
