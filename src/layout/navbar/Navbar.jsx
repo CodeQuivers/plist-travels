c5782be57ba78bda6c17a49baa96d954516dc3c2
MashodRana <mashod0rana@gmail.com>
2023-07-27 01:25:20 +0600

    [fixing] navbar

--- src/layout/navbar/Navbar.jsx
+++ src/layout/navbar/Navbar.jsx
@@ -24,66 +24,57 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="bg-[#EBF3FB] h-[92px] sticky top-0 z-50 ">
-      <div className="mx-auto py-[35px] flex items-center justify-center ">
-        <div>
+    <nav className="bg-[#EBF3FB] h-[88px] sticky top-0 z-50 flex items-center">
+      <div className="w-[1170px] mx-auto flex items-center justify-between">
+        <div className="w-1/12">
           <img src={logo} alt="" />
         </div>
-        <div className="flex gap-[23px] text-[#777E90] ml-[127px]">
-          <Link to="/">Home</Link>
-          <Link to="/hotel">Hotel/Stays</Link>
-          <Link to="/flight">Flight</Link>
-          <Link to="/tours">Tours</Link>
-          <Link to="/transfer">Transfer</Link>
-        </div>
-        <div className="flex gap-[24px] ml-[31px] items-center">
-          <div className=" btn-1 w-[116px] h-[40px] ">
-            <select
-              name=""
-              id=""
-              className=" bg-[#EBF3FB] text-[#777E90] text-[14px] font-medium text-center "
-            >
-              <option value="">
-                {" "}
-                <span className=" text-[black] w-[16px] h-[16px]">
-                  <RiMoneyDollarCircleLine />
-                </span>{" "}
-                <span>USD</span>
-              </option>
-            </select>
+        <div className="w-[82%] flex justify-between">
+          {/* home, hotel, flight, tours, transfer */}
+          <div className="flex items-center gap-6 text-[#777E90]">
+            <Link to="/">Home</Link>
+            <Link to="/hotel">Hotel/Stays</Link>
+            <Link to="/flight">Flight</Link>
+            <Link to="/tours">Tours</Link>
+            <Link to="/transfer">Transfer</Link>
           </div>
-          <div className="btn-2 w-[116px] h-[40px]">
-            <select name="" id="" className="text-[#777E90] text-[14px]">
-              <option value="">EG</option>
-            </select>
+
+          <div className="flex gap-4 items-center">
+            <div className=" btn-1 w-[116px] h-[40px] ">
+              <select
+                name=""
+                id=""
+                className=" bg-[#EBF3FB] text-[#777E90] text-[14px] font-medium text-center "
+              >
+                <option value="">
+                  <span className=" text-[black] w-[16px] h-[16px]">
+                    <RiMoneyDollarCircleLine />
+                  </span>
+                  <span>USD</span>
+                </option>
+              </select>
+            </div>
+            <div className="btn-2 w-[116px] h-[40px]">
+              <select name="" id="" className="text-[#777E90] text-[14px]">
+                <option value="">EG</option>
+              </select>
+            </div>
           </div>
           <div>
             <img
               src={avater}
-              alt=""
-              className="w-[40px] h-[40px] rounded-[40px] ]"
+              alt="avatar of the user"
+              className="w-10 h-10 rounded-full ]"
             />
           </div>
-        </div>
-        <div className="ml-[25px] flex items-center">
-          <button className="btn-3">Become an Agent</button>
-          {open && (
-            <img
-              src={subMenu}
-              alt=""
-              className="w-[36px] h-[36px] ml-[27px] "
-              onClick={() => setOpen(!open)}
-            />
-          )}
-
-          {!open && (
-            <img
-              src={subMenu}
-              alt=""
-              className="w-[36px] h-[36px] ml-[27px] "
-              onClick={() => setOpen(!open)}
-            />
-          )}
+          <div className="w-48 flex items-center">
+            <button className="old-logo-color px-6 py-4 rounded-xl font-bold text-white">Become an Agent</button>
+          </div>
+          <div className="w-9 flex items-center">
+            <button>
+              <img className="w-9 h-9" src={subMenu} alt="" />
+            </button>
+          </div>
         </div>
       </div>
 
