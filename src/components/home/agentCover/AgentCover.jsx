9406db5e27a713612ac870763d70086b70a0437e
MashodRana <mashod0rana@gmail.com>
2023-07-27 20:46:24 +0600

    [Done] become agent section

--- src/components/home/agentCover/AgentCover.jsx
+++ src/components/home/agentCover/AgentCover.jsx
@@ -8,19 +8,19 @@ const AgentCover = () => {
   return (
     <div className="w-full h-[470px] agent-bg">
       <div className="h-full agent-child-bg flex flex-col gap-[30px]">
-        <div className="w-full h-[340px] flex flex-col justify-center items-center">
-          <h2 className="max-w-[1086px] w-3/4 text-3xl font-bold pop-font text-white text-center">
+        <div className="w-full h-[340px] flex flex-col justify-center items-center gap-9 dmsan-font font-bold text-white">
+          <h2 className="max-w-[1086px] w-3/4 text-3xl text-center dmsan-font ">
             We partner with travel agents across the globe, bringing new 
             realms
             of luxury travel options to your clients.
           </h2>
           <div>
-            <button>Become An Agent</button>
+            <button className="px-6 py-4 old-logo-color rounded-lg">Become An Agent</button>
           </div>
         </div>
-        <div className=" h-[100px] rana mb-0">
+        <div className="mb-0 mt-auto">
           <img className="w-[2221px] overflow-hidden" src={parabolaShapeImg} alt="" />
-          <div className="h-[20px] bg-white"></div>
+          {/* <div className="h-[20px] bg-white"></div> */}
         </div>
       </div>
     </div>
