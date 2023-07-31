0c98df69299e22eb8f7f01d235f3bd6334ede764
MashodRana <mashod0rana@gmail.com>
2023-07-31 21:02:34 +0600

    [FIXED] tour faq section width

--- src/components/tours/TourFaq.jsx
+++ src/components/tours/TourFaq.jsx
@@ -3,20 +3,7 @@ import HotelFaq from "../hotel/HotelFaq";
 
 const TourFaq = () => {
   return (
-    <div className="xl:container mx-auto px-12 mt-5 sm:mt-8 lg:mt-[70px]">
-      {/* <div
-    className="
-        lg:mx-0
-        md:px-4
-        lg:px-0
-        flex 
-        flex-wrap
-        justify-center
-        md:justify-between
-        gap-y-5
-    "
-  >
-  </div> */}
+    <div className="w-[1170px] mx-auto my-20">
       <HotelFaq />
     </div>
   );
