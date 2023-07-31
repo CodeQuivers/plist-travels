51405f90a0be4318da57c467e11dc896c317a3b7
MashodRana <mashod0rana@gmail.com>
2023-07-31 20:09:48 +0600

    [FIXED] tour search box

--- src/components/tours/SearchTour.jsx
+++ src/components/tours/SearchTour.jsx
@@ -7,6 +7,8 @@ import bedBlackIcon from "../../assets/image/tours/icons/bed-black.svg";
 import calenderBlackIcon from "../../assets/image/tours/icons/calender-black.svg";
 import Select from "react-dropdown-select";
 
+import "./SearchTour.css";
+
 const SearchTour = () => {
   const [dateRange, setDateRange] = useState([null, null]);
   const [startDate, endDate] = dateRange;
@@ -14,23 +16,23 @@ const SearchTour = () => {
   const options = [
     {
       value: 1,
-      label: "1 Room(s) - 1 Guest(s)",
+      label: "1 adult",
     },
     {
       value: 2,
-      label: "1 Room(s) - 2 Guest(s)",
+      label: "2 adult",
     },
     {
       value: 3,
-      label: "2 Room(s) - 4 Guest(s)",
+      label: "3 adult",
     },
   ];
 
   return (
-    <div className="w-[1170px] mx-auto mt-20 py-16 border rounded-3xl border-[#287DFA] bg-[#FCFCFD]">
-      <div className="w-full flex gap-5 bg-white px-28 py-5">
-        <div className="w-[70%] flex gap-4">
-          <div className="flex flex-col gap-1.5">
+    <div className="tour-search-box w-[1170px] mx-auto mt-20 py-16 border rounded-3xl border-[#287DFA] bg-[#FCFCFD]">
+      <div className="w-full flex justify-center gap-5 bg-white px-9 py-5">
+        <div className="flex gap-4">
+          <div className="w-[250px] flex flex-col gap-1.5">
             <lable className="text-base font-semibold text-[#0D233E]">
               Destination
             </lable>
@@ -47,7 +49,7 @@ const SearchTour = () => {
             <lable className="text-base font-semibold text-[#0D233E]">
               Travel Date
             </lable>
-            <div className="flex items-center gap-1.5 gray-border px-3 py-3">
+            <div className="flex items-center gap-1.5 gray-border px-3 py-3 text-sm">
               <img
                 src={calenderBlackIcon}
                 className="w-[20px] h-[14px]"
@@ -67,19 +69,23 @@ const SearchTour = () => {
               />
             </div>
           </div>
-          <div className="flex flex-col gap-1.5">
+          <div className="w-[250px] flex flex-col gap-1.5">
             <lable className="text-base font-semibold text-[#0D233E]">
               Passengers
             </lable>
-            <div className="flex items-center gap-1.5 gray-border px-3 py-3">
-              <Select options={options} />
+            <div className="w-full flex text-sm mas-ran items-center gap-1.5 gray-border px-3 py-3">
+              <Select
+                options={options}
+              />
             </div>
           </div>
         </div>
-        <div className="w-1/5">
-          <button className="old-logo-color text-sm font-semibold py-4 px-20 text-white rounded">
+        <div className="w-[250px] flex items-end" >
+          <div className="w-full">
+          <button className="old-logo-color text-sm font-semibold py-3.5 px-20 text-white rounded">
             Search Now
-          </button>
+          </button> 
+          </div>
         </div>
       </div>
     </div>
