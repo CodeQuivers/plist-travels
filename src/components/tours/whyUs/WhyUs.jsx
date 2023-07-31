ea2b6c4c96451a447a905e8079295dfd984d1446
MashodRana <mashod0rana@gmail.com>
2023-07-31 20:54:41 +0600

    [FIXED] gap fixed batween card

--- src/components/tours/whyUs/WhyUs.jsx
+++ src/components/tours/whyUs/WhyUs.jsx
@@ -8,18 +8,9 @@ const WhyUs = () => {
     content = whyAs.map((obj, idx) => <CardWhyUs key={idx} idx={idx+1} cardInfo={obj} />);
   }
   return (
-    <div className="w-[1170px] mx-auto px-12">
+    <div className="w-[1170px] mx-auto">
       <div
-        className="
-            lg:mx-0
-            md:px-4
-            lg:px-0
-            flex 
-            flex-wrap
-            justify-center
-            md:justify-between
-            gap-y-5
-        "
+        className="flex justify-between"
       >
      {content}
       </div>
