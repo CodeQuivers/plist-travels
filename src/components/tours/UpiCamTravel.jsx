bc402eefb5d1461a23f4fac9c3d7d96a82becaf3
MashodRana <mashod0rana@gmail.com>
2023-07-31 20:55:59 +0600

    [FIXED] upi cam travels design

--- src/components/tours/UpiCamTravel.jsx
+++ src/components/tours/UpiCamTravel.jsx
@@ -2,19 +2,10 @@ import CardCamTravel from "./CardCamTravel";
 
 const UpiCamTravel = () => {
   return (
-    <div className="xl:container mx-auto px-12 mt-5 sm:mt-8 lg:mt-[70px]">
+    <div className="w-[1170px] mx-auto mt-5 sm:mt-8 lg:mt-[70px]">
         <h1 className="text-3xl font-bold mb-8 text-[#0F5DB3]">Destination Upi Cam Travel Tp Mpw</h1>
       <div
-        className="
-            lg:mx-0
-            md:px-4
-            lg:px-0
-            flex 
-            flex-wrap
-            justify-center
-            md:justify-between
-            gap-y-5
-        "
+        className="flex justify-between"
       >
         <CardCamTravel/>
         <CardCamTravel/>
