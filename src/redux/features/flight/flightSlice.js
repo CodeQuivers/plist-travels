c616946071acf519f140b3937d2741926a0e16d7
MashodRana <mashod0rana@gmail.com>
2023-08-06 22:56:29 +0600

    [DONE] flight serach action

--- src/redux/features/flight/flightSlice.js
+++ src/redux/features/flight/flightSlice.js
@@ -1,12 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  isSearchResultAvailable: true,
+};
 
 const flightSlice = createSlice({
   name: "flight",
   initialState,
-  reducers: {},
+  reducers: {
+    updateIsSearchResultAvailable: (state, action) => {
+      state.isSearchResultAvailable = action.payload;
+    },
+  },
 });
 
-export const {} = flightSlice.actions;
+export const { updateIsSearchResultAvailable } = flightSlice.actions;
 export default flightSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./features/apiSlice";
+import flightSliceReducer from "./features/flight/flightSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    flight: flightSliceReducer,
   },
   // devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddlewares) =>
