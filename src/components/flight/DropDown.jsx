a93c2a54467212c3cf0a2ac8e47122a6b34beee8
MashodRana <mashod0rana@gmail.com>
2023-08-06 19:25:55 +0600

    [re-designed] dropdown for flight

--- src/components/flight/DropDown.jsx
+++ src/components/flight/DropDown.jsx
@@ -1,6 +1,6 @@
 import Select from "react-dropdown-select";
 
-const DropDown = ({options, placeholder}) => {
+const DropDown = ({options, placeholder, setOnChange}) => {
   return (
     <div>
       <Select
@@ -8,6 +8,7 @@ const DropDown = ({options, placeholder}) => {
         className="mt-2 pl-10 border-2 h-12 rounded"
         options={options}
         placeholder={placeholder}
+        onChange={(val)=>setOnChange(val[0].value)}
       />
     </div>
   );
