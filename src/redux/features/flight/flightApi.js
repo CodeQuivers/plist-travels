ba5403e6a4369f80a47604ed93de6b86c7ac03f1
MashodRana <mashod0rana@gmail.com>
2023-08-06 16:20:09 +0600

    [DONE] getLoctionFlight query done

--- src/redux/features/flight/flightApi.js
+++ src/redux/features/flight/flightApi.js
@@ -3,10 +3,33 @@ import { apiSlice } from "../apiSlice";
 const flightApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getLocations: builder.query({
-      query: (term) =>
-        `/getLocations?action=getLocations&lang=en&limit=5&term=${term}`,
+      query: (term) => {
+        return {
+          url: `/getLocations`,
+          params: {
+            action: "getLocations",
+            lang: "en",
+            limit: 5,
+            term: term,
+          },
+        };
+      },
+    }),
+
+    getLocationsFlights: builder.query({
+      query: (term) => {
+        return {
+          url: "/getLocationsFlights",
+          params: {
+            locale: "en",
+            max: 7,
+            limit: 5,
+            term: term,
+          },
+        };
+      },
     }),
   }),
 });
 
-export const { useGetLocationsQuery } = flightApi;
+export const { useGetLocationsQuery, useGetLocationsFlightsQuery } = flightApi;
