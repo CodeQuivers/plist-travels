48e2ac9f6c594ed4fdf5a0affc8ba4de0f7b9204
MashodRana <mashod0rana@gmail.com>
2023-08-06 21:57:12 +0600

    [TEMPLATE] rtk slice for flight.

---
+++ src/redux/features/flight/flightSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const flightSlice = createSlice({
+  name: "flight",
+  initialState,
+  reducers: {},
+});
+
+export const {} = flightSlice.actions;
+export default flightSlice.reducer;
